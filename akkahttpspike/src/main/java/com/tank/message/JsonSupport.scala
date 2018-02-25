5abe19a3ce46fb7742b94618f86a68dca4a6a381
Tank <fuch@fullsample.com>
2018-02-25 21:26:38 +0800

    [fuchun] #Tech 找到添加自定义json解析的程序入口

--- akkahttpspike/src/main/java/com/tank/message/JsonSupport.scala
+++ akkahttpspike/src/main/java/com/tank/message/JsonSupport.scala
@@ -1,10 +1,10 @@
 package com.tank.message
 
 import org.json4s.{DefaultFormats, jackson}
-
+import  org.json4s.ext.JodaTimeSerializers._
 trait JsonSupport {
 
   implicit val serialization = jackson.Serialization
-  implicit val formats = DefaultFormats
+  implicit val formats = DefaultFormats ++ all
 
 }
